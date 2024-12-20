4df0c621cec371937b4f30073b12aae653fc1001
Aryan Panchal <aryan.nikolas@gmail.com>
2024-12-20 22:19:25 +0530

    changed cors

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ const port = process.env.PORT || 3000
 app.use(express.urlencoded({extended:true, limit:'10mb'}));
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({origin:"http://localhost:5173", credentials:true}))
+app.use(cors({origin:"https://forkfeast.vercel.app", credentials:true}))
 
 app.use("/api/user", userRoutes)
 app.use("/api/restaurant", restaurantRoutes)
