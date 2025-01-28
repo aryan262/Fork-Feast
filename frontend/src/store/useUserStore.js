a3445065831eafbd9bad226e742106bd0e1c4453
Aryan Panchal <aryan.nikolas@gmail.com>
2025-01-28 16:40:43 +0530

    chnged api end point

--- frontend/src/store/useUserStore.js
+++ frontend/src/store/useUserStore.js
@@ -3,7 +3,7 @@ import { createJSONStorage, persist } from "zustand/middleware";
 import axios from "axios";
 import { toast } from "sonner";
 
-const API_END_POINT = "http://localhost:3000/api/v1/user"
+const API_END_POINT = "https://forkfeastbackend.vercel.app/api/v1/user"
 axios.defaults.withCredentials = true;
 
 
